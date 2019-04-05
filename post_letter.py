f5197de65b5b8a71a6929e657b486ff9f3dfd3af
Shen, Fei <mathe.hacker@gmail.com>
2019-04-06 00:29:53 +0800

    update to python3, render mail from html/md/Rmd file

--- post_letter.py
+++ post_letter.py
@@ -1,15 +1,38 @@
 import argparse
-from subprocess import getoutput
-from yaml import load as yaml_load
-try:
-    from yaml import CLoader as Loader
-except ImportError:
-    from yaml import Loader
+from subprocess import getoutput, run
+from pathlib import Path
+
+from yaml import safe_load as load_yaml
 import requests
 
 
-def send_email(inpath, subject, config):
-    mail_text = getoutput('pandoc %s -f markdown -t html' % inpath)
+def render_mail(inpath: str):
+    '''Generate a html body from `inpath` file
+
+    Input file type can be html, markdown (.md), or RMarkdown (.Rmd)
+    '''
+    inpath = Path(inpath)
+
+    if inpath.suffix == '.html':
+        with open(inpath) as fin:
+            mail = fin.read()
+
+    elif inpath.suffix == '.Rmd':
+        cmd = f"R -e \"rmarkdown::render('{inpath}', 'blogdown::html_page', output_dir = '.')\""
+        run(cmd, shell=True)
+        out_file = Path(f'{inpath.stem}.html')
+        with open(out_file) as fin:
+            mail = fin.read()
+        out_file.unlink()
+
+    elif inpath.suffix == '.md':
+        mail = getoutput(f'pandoc {inpath} -f markdown -t html')
+
+    return mail
+
+
+def send_email(inpath: str, subject: str, config: dict):
+    mail_text = render_mail(inpath)
     api_url = config['mailgun'].get('api_url')
     api_key = config['mailgun'].get('api_key')
     res = requests.post(api_url, auth=('api', api_key), data={
@@ -29,13 +52,12 @@ if __name__ == "__main__":
     parser.add_argument('--serial', '-n', type=int)
     args = parser.parse_args()
 
-    with open(args.config) as conf:
-        config = yaml_load(conf, Loader=Loader)
+    config = load_yaml(open(args.config))
 
     if args.subject is None:
         if args.serial is None:
             raise ValueError('Please input newsletter subject or serial number')
-        subject = 'Selfhack Newsletter #%d' % args.serial
+        subject = config.get('subject', '#{}').format(args.serial)
     else:
         subject = args.subject
 
